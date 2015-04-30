2556fac4cb51a8cf794f55cf37ad529855ad3479
Duong Truong <duongtd@sololab.net>
2015-04-30 12:26:13 +0700

    first commit

---
+++ src/Sololab/Vimap/VimapServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Sololab\Vimap;
+
+use Illuminate\Support\ServiceProvider;
+
+class VimapServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('sololab/vimap');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
